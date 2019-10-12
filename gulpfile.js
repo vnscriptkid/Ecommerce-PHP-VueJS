9b721e0f162c1c98a2321bb4899e1915c4ed83f1
Thanh Nguyen <vnscriptkid@gmail.com>
2019-10-12 12:03:14 +0700

    Add gulp tasks to compile then bundle css and js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const elixir = require('laravel-elixir');
+
+elixir(mix => {
+  // use mix.sass to compile sass to css
+  mix.sass('app.scss', 'resources/assets/css');
+  // use mix.style to combine app.css + slick.css -> public/css/all.css
+  mix.styles(
+    [
+      './node_modules/slick-carousel/slick/slick.css',
+      'resources/assets/css/app.css'
+    ],
+    'public/css/all.css'
+  );
+  // use mix.script to combine jquery + foundation + slick  -> public/js/all.js
+  mix.scripts(
+    [
+      './node_modules/jquery/dist/jquery.min.js',
+      './node_modules/foundation-sites/dist/js/foundation.min.js',
+      './node_modules/slick-carousel/slick/slick.min.js'
+    ],
+    'public/js/all.js'
+  );
+});
