bb4d29df7d91c39a7cbf05b17d4bdd7e8b5ee25e
Thanh Nguyen <vnscriptkid@gmail.com>
2019-10-14 17:12:20 +0700

    Add custom Csrf class to prevent the attack

---
+++ app/classes/CsrfToken.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Classes;
+
+class CsrfToken {
+  public static function _getOrCreate() {
+    if (!Session::has('csrfToken')) {
+      $csrfToken = \base64_encode(\openssl_random_pseudo_bytes(32));
+      Session::add('csrfToken');
+    }
+    return Session::get('csrfToken');
+  }
+
+  public static function verify($tokenToCheck) {
+    if (Session::has('csrfToken') && Session::get('csrfToken') == $tokenToCheck) {
+      return true;
+    }
+    return false;
+  }
+}
